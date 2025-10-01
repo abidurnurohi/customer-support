8cee74a8382ccfa8c95cef887e251ad3b4a196b9
abidurnurohi <abidurnurohi@gmail.com>
2025-10-01 20:49:35 +0700

    2nd

--- src/components/SelectedTickets/SelectedTickets.jsx
+++ src/components/SelectedTickets/SelectedTickets.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SelectedTickets = ({ticket, handleTicket}) => {
+const SelectedTickets = ({ ticket, handleTicket }) => {
     return (
         <div onClick={() => handleTicket(ticket)}
             className="text-black bg-white shadow-sm p-4 rounded-md h-full flex flex-col justify-between"
@@ -9,14 +9,25 @@ const SelectedTickets = ({ticket, handleTicket}) => {
             <div>
                 <div className="flex justify-between items-center">
                     <h2 className="font-semibold">{ticket.title}</h2>
-                    <span className="bg-green-300 rounded-full px-3">{ticket.status}</span>
+                    <span className={`px-3 rounded-full 
+                            ${ticket.status === 'Open' ? 'bg-green-300 text-green-800' :
+                            ticket.status === 'Resolved' ? 'bg-blue-300 text-blue-800' :
+                                ticket.status === 'In Progress' ? 'bg-yellow-200 text-yellow-800' : ''}`}>
+                        {ticket.status}
+                    </span>
                 </div>
                 <p>{ticket.description}</p>
             </div>
             <div className="flex justify-between mt-4">
                 <div className="flex gap-2">
                     <p>#{ticket.id}</p>
-                    <p>{ticket.priority}</p>
+                    <span className={`
+                            ${ticket.priority === 'High' ? 'text-red-500' :
+                            ticket.priority === 'Low' ? 'text-green-500' :
+                            ticket.priority === 'Critical' ? 'text-purple-500' :
+                            ticket.priority === 'Medium' ? 'text-yellow-500' : ''}`}>
+                        {ticket.priority}
+                    </span>
                 </div>
                 <div className="flex gap-4">
                     <p>{ticket.customer}</p>
